8548a77eff9d3d8b2ffef402bd3a19c3ce90ed67
alamnr <alamnr@gmail.com>
2021-10-05 13:21:28 +0600

    When do you want to use @SpringBootTest and Mockito manual, annotation and extension inject mock test

--- module04-question38/src/test/java/com/spring/professional/exam/tutorial/module04/question38/HelloControllerTest.java
+++ module04-question38/src/test/java/com/spring/professional/exam/tutorial/module04/question38/HelloControllerTest.java
@@ -5,11 +5,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
